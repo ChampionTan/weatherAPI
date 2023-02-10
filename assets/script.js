c3c31b7abb399e7da5c635cc25eb7334bdc3e57e
unknown <tannerwlumsden@gmail.com>
2023-02-10 11:03:45 -0500

    started over

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,3 @@
 //weather api 
 //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
-var inputCity = document.getElementById('#search');
-var apiKey = '07ebbc07eb71efd6931662d229d381e5';
-
-function fetchWeather(currentCity) {
-	var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='+ {inputCity} +'&appid='+ {apiKey};
-
-	fetch(apiUrl)
-	.then(function(response){
-		return response.json();
-	})
-	.then(function(data) {
-		console.log(data);
-	})
-	.catch(function(error) {
-		console.log(error);
-	})
-}
