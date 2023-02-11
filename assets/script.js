977eb491ec6cf0e59b1db906ec51eac976603341
unknown <tannerwlumsden@gmail.com>
2023-02-11 11:32:01 -0500

    started js

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,8 @@
 //weather api 
 //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
+var key = "07ebbc07eb71efd6931662d229d381e5";
+var weatherApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + key;
+var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + key;
+
+//function to display the current weather
